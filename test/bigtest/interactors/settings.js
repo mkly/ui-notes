573bb8a672c7ef8a2739257c0ff60572408e3cd5
Igor Godlevskyi <igor_godlevskyi@epam.com>
2019-04-16 15:40:36 +0300

    Removed initial stripes-cli tests

--- test/bigtest/interactors/settings.js
+++
@@ -1,8 +0,0 @@
-import {
-  interactor,
-  text
-} from '@bigtest/interactor';
-
-export default @interactor class SettingsInteractor {
-  generalMessage = text('[data-test-application-settings-general-message]');
-}
